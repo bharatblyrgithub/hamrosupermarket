21eb1341e5625828bd3ac323fa88450ea7f0bdba
Bharat Balayar <balayarbharat35@gmail.com>
2025-02-25 17:38:46 +0545

    delete the search functionality

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useSession, signOut } from 'next-auth/react';
-import { useCart } from '@/context/CartContext';
+import { useCart } from '../../context/CartContext';
 import CartDropdown from '../cart/CartDropdown';
 
 const Header = () => {
@@ -46,14 +46,6 @@ const Header = () => {
 
           {/* Right Side Icons */}
           <div className="flex items-center space-x-4">
-            {/* Search */}
-            <Link href="/search" className="text-gray-700 hover:text-green-600">
-              <span className="sr-only">Search</span>
-              <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-              </svg>
-            </Link>
-
             {/* Cart */}
             <div className="relative">
               <button
